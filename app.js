e0f112a6c1dae79f4c4816b12582700f15e2602a
baimamboukar <baimamboukar@gmail.com>
2022-01-19 21:00:38 +0100

    abi added

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-window.ethereum.enable();
+window.ethereum.enable()
 
 var provider = new ethers.providers.Web3Provider(
     web3.currentProvider,
     "ropsten"
   );
+
+  var MoodContractAddress = "/0x7cFC27b4b8e6A88664CC36A6dD3AFd0cC376fb3f"
+  var MoodContractABI = [
+	{
+		"inputs": [],
+		"name": "getMood",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_mood",
+				"type": "string"
+			}
+		],
+		"name": "setMood",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+  var MoodContract
+  var signer
+
+
+  provider.listAccounts().then(function (accounts) {
+    signer = provider.getSigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+      MoodContractAddress,
+      MoodContractABI,
+      signer
+    );
+  });
